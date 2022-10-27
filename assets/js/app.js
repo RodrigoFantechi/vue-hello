49c856e08a7dcdff21a57fa1088415d888cd6159
RodrigoFantechi <rodrifante595@libero.it>
2022-10-27 16:05:25 +0200

    add dynamic arguments

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,8 @@ const { createApp } = Vue
     data() {
       return {
         message: 'Hello Vue!',
-        url: 'https://picsum.photos/300/200'
+        url: 'https://picsum.photos/300/200',
+        src: 'src'
       }
     }
   }).mount('#app')
